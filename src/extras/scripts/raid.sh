2842e8915dbaede1e5fdf5f34e60416c86228577
tomcdj71 <contact.tomc@yahoo.fr>
2024-03-06 20:27:09 +0100

    chore: use mflibs::shell::prompt::yn for the 'Continue' prompt

--- src/extras/scripts/raid.sh
+++ src/extras/scripts/raid.sh
@@ -26,7 +26,7 @@
 # @arg $4 string Optional. The name of the RAID array. Default: md0.
 # @exitcode 0 Success.
 # @exitcode 1 Error due to incorrect number of arguments, invalid RAID level, invalid filesystem type, insufficient number of disks, or user cancellation.
-function raid::process::args() {
+raid::process::args() {
     # Check if the number of arguments is not equal to 4
     if [ "$#" -ne 4 ]; then
         echo "Usage: $0 [RAID_LEVEL] [MOUNT_POINT] [FILESYSTEM_TYPE] [DISK_NAME]"
@@ -110,10 +110,10 @@ function raid::process::args() {
 # This detection is crucial to determine which disks are available for formatting and inclusion in the new RAID array.
 # The function sets global variables for the disks to be formatted and their count.
 # @stdout Informs about the system disk, disks to be formatted, and their count.
-function raid::disk::detection() {
+raid::disk::detection() {
     local raid_packages
     raid_packages=("mdadm" "parted" "util-linux")
-    zen::dependency::apt::install::inline "${raid_packages[@]}"
+    zen::dependency::apt::install::inline "${raid_packages[*]}"
     ROOT_DEVICE=$(findmnt -n -o SOURCE --target /)
     if [[ $ROOT_DEVICE == /dev/md* ]]; then
         SYSTEM_DISK=$ROOT_DEVICE
@@ -122,7 +122,7 @@ function raid::disk::detection() {
         SYSTEM_DISK=$(lsblk -no PKNAME "$ROOT_DEVICE")
     fi
 
-    mflibs::status::info "$(zen::i18n::translate "raid.system_disk" "$SYSTEM_DISK")"
+    mflibs::status::info "$(zen::i18n::translate "raid.system_disk" "/dev/$SYSTEM_DISK")"
     DISKS_TO_FORMAT=()
     for disk in $(lsblk -nd -o NAME,TYPE | awk '$2 == "disk" {print $1}'); do
         disk_path="/dev/$disk"
@@ -143,30 +143,18 @@ function raid::disk::detection() {
 # @description This function prompts the user to confirm disk formatting and proceeds to wipe and partition each disk in preparation for RAID array creation.
 # It ensures that each disk is properly prepared and partitioned with the specified filesystem type.
 # @stdout Guides the user through disk formatting process and reports on the status of each disk.
-function raid::format::disk(){
+raid::format::disk(){
     echo ""
     mflibs::shell::icon::warning::yellow;mflibs::shell::text::yellow::sl "Continue ? [";mflibs::shell::text::green::sl "Y";mflibs::shell::text::yellow::sl "] or [";mflibs::shell::text::red::sl "N";mflibs::shell::text::yellow::sl "] (default : ";mflibs::shell::text::red::sl "N";mflibs::shell::text::yellow::sl " )"
-    echo -ne "[Y] >";mflibs::shell::text::green " Continue"
-    echo -ne "[N] >";mflibs::shell::text::red " Cancel"
-    echo -en "> "
-    read -r startraid
-    case $startraid in
-        [yY]) formatdisk=1 ;;
-        [nN] | "") formatdisk=0 ;;
-        *) formatdisk=0 ;;
-    esac
+    mflibs::shell::prompt::yn "Continue ?" N || { mflibs::status::warn "$(zen::i18n::translate "raid.creation_aborted")"; exit 1; }
 
-    if [[ $formatdisk = 1 ]]; then
-        mflibs::status::header "$(zen::i18n::translate "raid.creating_partitions_empty_disks")"
-        for disk in "${DISKS_TO_FORMAT[@]}"; do
-            wipefs -a "$disk" &> /dev/null || { mflibs::status::error "$(zen::i18n::translate "raid.error_formatting_disk" "$disk")"; continue; }
-            parted -s "$disk" mklabel msdos mkpart primary "$filesystem_type" 1 100% &> /dev/null || { mflibs::status::error "$(zen::i18n::translate "raid.error_creating_partition" "$disk")"; continue; }
-            sleep 3
-        done
-        mflibs::status::success "$(zen::i18n::translate "raid.disk_partitions_created")"
-    else
-        mflibs::status::error "$(zen::i18n::translate "raid.disk_partitions_not_created")"
-    fi
+    mflibs::status::header "$(zen::i18n::translate "raid.creating_partitions_empty_disks")"
+    for disk in "${DISKS_TO_FORMAT[@]}"; do
+        wipefs -a "$disk" &> /dev/null || { mflibs::status::error "$(zen::i18n::translate "raid.error_formatting_disk" "$disk")"; continue; }
+        parted -s "$disk" mklabel msdos mkpart primary "$filesystem_type" 1 100% &> /dev/null || { mflibs::status::error "$(zen::i18n::translate "raid.error_creating_partition" "$disk")"; continue; }
+        sleep 3
+    done
+    mflibs::status::success "$(zen::i18n::translate "raid.disk_partitions_created")"
     sleep 5
 }
 
@@ -176,7 +164,7 @@ function raid::format::disk(){
 # It handles different RAID levels and configures the RAID array accordingly.
 # The RAID array is created using the disks that were formatted in the previous step.
 # @stdout Details the RAID creation process and reports any errors encountered.
-function raid::create::mdadm::disk(){
+raid::create::mdadm::disk(){
     local data_raid_level
     local metadata_raid_level
     local fs_type_upper
@@ -228,7 +216,7 @@ function raid::create::mdadm::disk(){
 # @description After creating the RAID array, this function mounts it to the provided mount point.
 # It also updates the /etc/fstab file to ensure the RAID array is mounted automatically on boot.
 # @stdout Indicates the mounting process of the RAID array and updates the /etc/fstab file.
-function raid::mount::mdadm::disk(){
+raid::mount::mdadm::disk(){
     local RAID_UUID
     RAID_UUID=$(find /dev/disk/by-uuid/ -maxdepth 1 -type l -name 'md*' -printf '%l\n' | sed 's/ ->.*//')
 	
