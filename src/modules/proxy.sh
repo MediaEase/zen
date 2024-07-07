1ed20d59bcfb90e93f17e81b8454821e242f1a82
tomcdj71 <contact.tomc@yahoo.fr>
2024-07-07 09:02:58 +0200

    style: minor formatting edit

--- src/modules/proxy.sh
+++ src/modules/proxy.sh
@@ -11,8 +11,8 @@
 
 # @function zen::proxy::generate
 # Generates a Caddy proxy configuration file for an application.
-# @description This function creates a Caddy configuration file for a specified application. 
-# It generates the configuration file based on the application name, port number, and base URL. 
+# @description This function creates a Caddy configuration file for a specified application.
+# It generates the configuration file based on the application name, port number, and base URL.
 # The configuration will differ based on whether the application is in multi-user mode or not.
 # @arg $1 string The name of the application.
 # @arg $2 number The port on which the application is running.
@@ -31,13 +31,13 @@ zen::proxy::generate() {
 	is_multi=$(zen::software::get_config_key_value "$software_config_file" '.arguments.multi_user')
 	[[ ! -d "$(dirname "$caddy_file")" ]] && mkdir -p "$(dirname "$caddy_file")"
 	if [ "$is_multi" == "true" ]; then
-			caddy_file="/etc/caddy/softwares/${user[username]}.$app_name.caddy"
+		caddy_file="/etc/caddy/softwares/${user[username]}.$app_name.caddy"
 	else
-			caddy_file="/etc/caddy/softwares/$app_name.caddy"
+		caddy_file="/etc/caddy/softwares/$app_name.caddy"
 	fi
 	[[ ! -d "$(dirname "$caddy_file")" ]] && mkdir -p "$(dirname "$caddy_file")"
 	# Write configuration to the file
-	cat <<EOF > "${caddy_file}"
+	cat <<EOF >"${caddy_file}"
 	route ${url_base}/* {
 		reverse_proxy 127.0.0.1:$port {
 			header_up -Accept-Encoding
@@ -50,7 +50,7 @@ EOF
 
 # @function zen::proxy::add_directive
 # Adds a directive to an application's Caddy proxy configuration.
-# @description This function appends a new directive to the Caddy proxy configuration of a specified application. 
+# @description This function appends a new directive to the Caddy proxy configuration of a specified application.
 # It is useful for adding custom rules or modifications to the existing proxy settings.
 # @arg $1 string The name of the application.
 # @arg $2 string The username associated with the application (used in multi-user mode).
@@ -67,12 +67,12 @@ zen::proxy::add_directive() {
 	caddy_file=$(zen::software::get_config_key_value "$software_config_file" '.arguments.files[] | select(has("proxy")).proxy' "${user[username]}" "$app_name")
 
 	# Append the directive to the file
-	echo "$directive" >> "${caddy_file}"
+	echo "$directive" >>"${caddy_file}"
 }
 
 # @function zen::proxy::remove
 # Removes the proxy configuration file for a specified application.
-# @description This function deletes the Caddy proxy configuration file associated with a given application. 
+# @description This function deletes the Caddy proxy configuration file associated with a given application.
 # It's used when an application's proxy is no longer needed or when the application is being uninstalled or moved.
 # @arg $1 string The name of the application.
 # @arg $2 string The username associated with the application (used in multi-user mode).
