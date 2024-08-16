66293adafc47e84732c877fe268c8a032c10f97a
tomcdj71 <contact.tomc@yahoo.fr>
2024-08-17 00:28:51 +0200

    chore: move common::git function to a new git module

--- src/modules/common.sh
+++ src/modules/common.sh
@@ -9,180 +9,6 @@
 # @copyright Copyright (C) 2024, Thomas Chauveau
 # All rights reserved.
 
-# @section Git Functions
-# @description The following functions handle Git operations.
-
-# @function zen::common::git::clone
-# Clones a Git repository into the specified directory.
-# @description This function clones a Git repository from a given URL into a specified directory.
-# It checks if the directory already exists to avoid re-cloning. Optionally, a specific branch can be cloned.
-# @arg $1 string Full URL of the Git repository to clone.
-# @arg $2 string Target directory where the repository will be cloned.
-# @arg $3 string Specific branch to clone (optional).
-# @arg $4 bool Recursively clone submodules (optional).
-# @exitcode 0 on successful cloning.
-# @exitcode 1 on failure.
-# @stdout Informs about the cloning process and results.
-zen::common::git::clone() {
-	local repo_name="$1"
-	local target_dir="$2"
-	local branch="${3}"
-	local recurse_submodules="${4:-false}"
-	local repo_url
-	repo_url="https://github.com/$repo_name"
-	if [ -d "$target_dir" ]; then
-		mflibs::status::warn "$(zen::i18n::translate "errors.common.env_variable_missing" "$repo_url")"
-		return 0
-	fi
-
-	if [ -z "$branch" ]; then
-		branch=$(git ls-remote --symref "$repo_url" HEAD | awk '/^ref:/ {sub(/refs\/heads\//, "", $2); print $2}')
-	fi
-
-	if [ "$recurse_submodules" == "true" ]; then
-		recurse_submodules="--recurse-submodules"
-	else
-		recurse_submodules=""
-	fi
-
-	if mflibs::log "git clone --branch $branch $repo_url $target_dir $recurse_submodules"; then
-		mflibs::status::success "$(zen::i18n::translate "messages.common.release_found" "$repo_url")"
-
-		local username
-		local group
-
-		if [[ "$target_dir" == /opt/* && "$target_dir" != /opt/pyenv* && "$target_dir" != /opt/MediaEase* ]]; then
-			username=$(echo "$target_dir" | cut -d'/' -f3)
-			group=$(getent group | grep "^$username:" | cut -d: -f1)
-		else
-			username="www-data"
-			group="www-data"
-		fi
-
-		zen::permission::fix "$target_dir" "755" "644" "$username" "$group"
-
-		if [[ "$target_dir" == /opt/pyenv* || "$target_dir" == /opt/MediaEase* ]]; then
-			zen::permission::fix "$target_dir" "755" "644" "www-data" "www-data"
-		fi
-	else
-		mflibs::status::error "$(zen::i18n::translate "errors.common.repository_clone" "$repo_url")"
-		return 1
-	fi
-}
-
-# @function zen::common::git::get_release
-# Retrieves and extracts a release from a GitHub repository.
-# @description This function downloads and extracts a specific release (stable or prerelease) from a GitHub repository.
-# It supports various file types for the release archive and sets appropriate permissions for the extracted files.
-# @tip Check the release file type (alpha, beta, rc, ...) to ensure it is supported before downloading.
-# @arg $1 string Directory where the release will be extracted.
-# @arg $2 string Full URL of the GitHub repository.
-# @arg $3 bool Retrieve a prerelease (true) or stable release (false).
-# @arg $4 string Name or pattern of the release file to be retrieved.
-# @exitcode 0 on successful retrieval and extraction.
-# @exitcode 1 on failure.
-# @stdout Details the process of downloading and extracting the release.
-zen::common::git::get_release() {
-	local target_dir="$1"
-	local repo_name="$2"
-	local is_prerelease="$3"
-	local release_name="$4"
-	local repo_url
-	mflibs::shell::text::white "$(zen::i18n::translate "messages.common.downloading_release" "$repo_name")"
-	repo_url="https://api.github.com/repos/$repo_name/releases"
-	release_url="$(curl -s "$repo_url" | jq -r "[.[] | select(.prerelease == $is_prerelease)] | first | .assets[] | select(.name | endswith(\"$release_name\")).browser_download_url")"
-	declare -g release_version
-	release_version=$(echo "$release_url" | grep -oP '(?<=/download/)[^/]+(?=/[^/]+$)')
-	mflibs::shell::text::white::sl "$(mflibs::shell::text::cyan "$(zen::i18n::translate "messages.common.release_found" "$repo_name"): $release_version")"
-	[[ -d $target_dir ]] && rm -rf "$target_dir"
-	mflibs::dir::mkcd "$target_dir"
-	wget -q "$release_url"
-	local downloaded_file
-	downloaded_file="$(basename "$release_url")"
-	mflibs::file::extract "$downloaded_file"
-	rm -f "$downloaded_file"
-	if [[ "$target_dir" == /opt/* ]]; then
-		local username
-		username=$(echo "$target_dir" | cut -d'/' -f3)
-		local group
-		group=$(getent group | grep "^$username:" | cut -d: -f1)
-
-		zen::permission::read_exec "$target_dir" "$username" "$group"
-	else
-		zen::permission::read_exec "$target_dir" "www-data" "www-data"
-	fi
-	mflibs::shell::text::green "$(zen::i18n::translate "success.common.release_downloaded" "$repo_name")"
-}
-
-# @function zen::common::git::download_file
-# Downloads a specific file from a GitHub repository.
-# @description This function downloads a specified file from a given GitHub repository and saves it to a local path.
-# @arg $1 string Local path where the file should be saved.
-# @arg $2 string Name of the repository (e.g., "git/core").
-# @arg $3 string Branch of the repository to download from.
-# @arg $4 string Path to the remote file in the repository.
-# @exitcode 0 on successful download.
-# @exitcode 1 on failure.
-# @stdout Informs about the downloading process and results.
-zen::common::git::download_file() {
-	local local_path="$1"
-	local repo_name="$2"
-	local branch="$3"
-	local remote_file="$4"
-	local repo_url="https://raw.githubusercontent.com/$repo_name/$branch/$remote_file"
-
-	# Check if curl is installed
-	if ! command -v curl &>/dev/null; then
-		mflibs::status::error "$(zen::i18n::translate "errors.dependency.dependency_missing" "cUrl")"
-	fi
-
-	# Download the file
-	if curl -o "$local_path" "$repo_url"; then
-		mflibs::status::success "$(zen::i18n::translate "success.common.file_downloaded" "$repo_url" "$local_path")"
-	else
-		mflibs::status::error "$(zen::i18n::translate "errors.common.file_download" "$repo_url")"
-	fi
-}
-
-# @function zen::common::git::tree
-# Lists the files in a given repository and branch.
-# @description This function lists the files in a specified repository and branch using the GitHub API.
-# @tip Use appropriate filters to list only the files you are interested in.
-# @arg $1 string Remote path to the directory in the repository.
-# @arg $2 string Name of the repository (e.g., "MediaEase/binaries").
-# @arg $3 string Branch name (e.g., "main").
-# @exitcode 0 on successful retrieval and listing.
-# @exitcode 1 on failure.
-# @stdout Lists the files in the specified directory.
-zen::common::git::tree() {
-	local remote_path="$1"
-	local repo_name="$2"
-	local branch="$3"
-	local api_url="https://api.github.com/repos/$repo_name/contents/$remote_path?ref=$branch"
-
-	# Check if curl and jq are installed
-	if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null; then
-		mflibs::status::error "$(zen::i18n::translate "errors.common.required_tools_missing")"
-	fi
-
-	# Fetch and list the files
-	local response
-	response=$(curl -s "$api_url")
-
-	# Check if response is empty or not a valid array
-	if [ -z "$response" ] || ! echo "$response" | jq -e . >/dev/null 2>&1; then
-		mflibs::status::error "$(zen::i18n::translate "common.invalid_api_response" "$response")"
-	fi
-
-	echo "$response" | jq -r '.[] | "\(.type)\t\(.name)"' | while IFS=$'\t' read -r type name; do
-		if [[ $type == "file" ]]; then
-			mflibs::shell::text::green "$name"
-		elif [[ $type == "dir" ]]; then
-			mflibs::shell::text::blue "$name/"
-		fi
-	done
-}
-
 # @section Environment Functions
 # @description The following functions are used for environment variable management.
 
@@ -218,6 +44,38 @@ zen::common::environment::set::variable() {
 
 	if ! grep -q "export $var_name=" "$bashrc_file"; then
 		echo "export $var_name=\"$var_value\"" >>"$bashrc_file"
+	fi
+}
+
+# @function zen::common::bashrc::append
+# Appends given lines to the .bashrc file for a specified user.
+# @description This function appends specified lines to the .bashrc file for a specified user, ensuring that each line is present
+# in future shell sessions. It is useful for adding custom environment variables or aliases to the shell environment.
+# @arg $1 array Lines to be appended to the .bashrc file.
+# @arg $2 string (optional) Username whose .bashrc file will be modified. Defaults to the current user if not provided.
+# @exitcode 0 on successful appending.
+# @exitcode 1 on failure.
+# @stdout None.
+zen::common::bashrc::append() {
+	local -n lines="$1"
+	local user="${2:-$USER}"
+	local bashrc_file
+
+	if [ "$user" = "root" ]; then
+		bashrc_file="/root/.bashrc"
+	else
+		bashrc_file="/home/$user/.bashrc"
+	fi
+
+	if [ -f "$bashrc_file" ]; then
+		for line in "${lines[@]}"; do
+			if ! grep -qF "$line" "$bashrc_file"; then
+				echo "$line" >>"$bashrc_file"
+			fi
+		done
+	else
+		mflibs::status::error "$(zen::i18n::translate "errors.common.bashrc_missing" "$bashrc_file")"
+		return 1
 	fi
 }
 
@@ -330,6 +188,9 @@ zen::common::make::install() {
 	fi
 }
 
+# @section Building tools Functions
+# @description The following functions are used for building tools.
+
 # @function zen::common::scons::install
 # Installs a project using the scons build system.
 # @description This function handles the configuration, building, and installation of a project that utilizes scons as its build system.
@@ -362,36 +223,4 @@ zen::common::scons::install() {
 		mflibs::status::error "$(zen::i18n::translate "errors.common.scons_install" "$source_dir")"
 	fi
 	mflibs::status::success "$(zen::i18n::translate "errors.common.scons_install" "$source_dir")"
-}
-
-# @function zen::common::bashrc::append
-# Appends given lines to the .bashrc file for a specified user.
-# @description This function appends specified lines to the .bashrc file for a specified user, ensuring that each line is present
-# in future shell sessions. It is useful for adding custom environment variables or aliases to the shell environment.
-# @arg $1 array Lines to be appended to the .bashrc file.
-# @arg $2 string (optional) Username whose .bashrc file will be modified. Defaults to the current user if not provided.
-# @exitcode 0 on successful appending.
-# @exitcode 1 on failure.
-# @stdout None.
-zen::common::bashrc::append() {
-	local -n lines="$1"
-	local user="${2:-$USER}"
-	local bashrc_file
-
-	if [ "$user" = "root" ]; then
-		bashrc_file="/root/.bashrc"
-	else
-		bashrc_file="/home/$user/.bashrc"
-	fi
-
-	if [ -f "$bashrc_file" ]; then
-		for line in "${lines[@]}"; do
-			if ! grep -qF "$line" "$bashrc_file"; then
-				echo "$line" >>"$bashrc_file"
-			fi
-		done
-	else
-		mflibs::status::error "$(zen::i18n::translate "errors.common.bashrc_missing" "$bashrc_file")"
-		return 1
-	fi
 }
