206be9fa28499aa54596efcafe67f85ce5784f07
tomcdj71 <contact.tomc@yahoo.fr>
2025-02-21 20:59:43 +0100

    docs: update documentation for v0.88.9-alpha [automated]

--- src/tools/quota.sh
+++ src/tools/quota.sh
@@ -2,7 +2,7 @@
 # @file zen/src/tools/quota.sh
 # @project MediaEase
 # @description Quota installation and management tool
-# @version 1.1.11
+# @version 1.1.4
 # @author Thomas Chauveau (tomcdj71)
 # @author_contact thomas.chauveau.pro@gmail.com
 # @license BSD-3 Clause (Included in LICENSE)
